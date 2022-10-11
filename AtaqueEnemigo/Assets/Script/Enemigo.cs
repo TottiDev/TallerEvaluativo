8469187737a6a75544ec7655aabe4c7b5aa9d09c
JuanjoEV <juanjoecheverri48@gmail.com>
2022-10-11 16:51:58 -0500

    Ataque

---
+++ AtaqueEnemigo/Assets/Script/Enemigo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Enemigo : MonoBehaviour
+{
+
+    [SerializeField] private int vidaTotal;
+    //public TextMeshProUGUI puntosVida;
+
+    void start()
+    {
+        
+        //puntosVida.text = " Puntos de vida " + vidaTotal.ToString();
+
+    }
+
+    
+
+
+}
